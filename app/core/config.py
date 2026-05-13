fc2e93f1128b6722d374f958561cba67571fd987
Vasukhnik <Vasukhnik@gmail.com>
2026-05-13 14:55:13 +0300

    lab4 complete

--- app/core/config.py
+++ app/core/config.py
@@ -2,27 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
+
+    DATABASE_URL: str
+    ALEMBIC_DATABASE_URL: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore"
     )
-
-    @property
-    def DATABASE_URL(self):
-        return (
-            f"postgresql+asyncpg://"
-            f"{self.POSTGRES_USER}:"
-            f"{self.POSTGRES_PASSWORD}@"
-            f"{self.POSTGRES_HOST}:"
-            f"{self.POSTGRES_PORT}/"
-            f"{self.POSTGRES_DB}"
-        )
 
 
 settings = Settings()
